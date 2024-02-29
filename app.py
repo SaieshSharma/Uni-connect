311957f4049512018d5aab8a87458d33913ae919
SaieshSharma <161768278+SaieshSharma@users.noreply.github.com>
2024-02-29 21:52:46 +0000

    added pics

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask   # flask is a module and Flask is a class
+from flask import Flask, render_template  # flask is a module and Flask is a class
 app = Flask(__name__)              # It is an object of that class
 
 #print(__name__)  this will print name
 
 @app.route('/')     # registering a route
 def hello_world():           # It is colon, we have created a function
-  return "Hello World" 
+  return render_template('home.html')
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', debug = True)
